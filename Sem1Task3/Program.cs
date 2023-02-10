14bb2301c55a554a9e98e264932c89f67f5285cc
Th3_Fp3e <dartneket@yandex.ru>
2023-02-10 22:09:16 +0300

    Task 3 - full line

---
+++ Sem1Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача №5
+// Напишите программу, которая на вход принимает одно число (N), 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.Write("Введите ваше число: ");
+string? lineN = Console.ReadLine();
+
+int N = int.Parse(lineN);
+
+string ves = String.Empty;
+int negN = N * (-1);
+
+while (negN < N)
+{
+    ves = ves + negN + ", ";
+    negN++;
+}
+ves = ves + N;
+Console.WriteLine(ves);
