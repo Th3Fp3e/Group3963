79439a1660779c98610801aa929bcbe07d9adf5a
Th3_Fp3e <dartneket@yandex.ru>
2023-02-10 22:32:52 +0300

    Homework - Task 2, the biggest number out of two

---
+++ Homework/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: 
+// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число: ");
+string? lineNum1 = Console.ReadLine();
+Console.Write("Введите второе число: ");
+string? lineNum2 = Console.ReadLine();
+
+int num1 = int.Parse(lineNum1);
+int num2 = int.Parse(lineNum2);
+
+if (num1 > num2)
+{
+    Console.WriteLine("Первое число больше!");
+}
+else
+{
+    Console.WriteLine("Второе число больше!");
+}
