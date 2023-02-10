5437ed5fcd4b57954cd9c4fd29ca569fb85d609b
Th3_Fp3e <dartneket@yandex.ru>
2023-02-10 20:48:06 +0300

    Task 1 - Squared Number

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1
+// Написать программу, которая принамает число и выдает его квадрат
+
+// Считываем данных с консоли
+Console.Write("Введите число: ");
+string? inputNum = Console.ReadLine();
+
+// Проверяем, чтобы данные не были пустыми
+if (inputNum != null)
+{
+    // Парсим введенное число
+    int number = int.Parse(inputNum);
+
+    // Находим квадрат числа
+    int outNum = (int)Math.Pow(number, 2);
+
+    // Выводим квадрат числа
+    Console.Write("Квадрат вашего числа - ");
+    Console.WriteLine(+outNum);
+}
