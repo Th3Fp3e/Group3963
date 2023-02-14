271c386add409b3b9f4c51475e8e6f73f8a71350
Th3_Fp3e <dartneket@yandex.ru>
2023-02-14 17:26:10 +0300

    Task 8 - evens from 1 to N

---
+++ Homework/Task 8/Program.cs
@@ -0,0 +1,62 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = index + 1;
+        index++;
+    }
+}
+
+void PrintEvenArray(int[] col)
+{
+    int count = col.Length;
+    int position = 3;
+    if (count > 1) Console.Write("2");
+    while (position < count)
+    {
+        if (col[position] % 2 == 0)
+        {
+            Console.Write(", " + col[position]);
+        }
+        position++;
+    }
+}
+
+// Либо:
+// void PrintEvenArray(int[] col)
+// {
+//     int count = col.Length;
+//     int position = 0;
+//     while (position < count) 
+//     {
+//         if (col[position] % 2 == 0) 
+//         {
+//             Console.Write(col[position]);
+//             position
+//             break;
+//         }
+//         position++;
+//     }
+//     while (position < count)
+//     {
+//         if (col[position] % 2 == 0)
+//         {
+//             Console.Write(", " + col[position]);
+//         }
+//         position++;
+//     }
+// }
+
+Console.Write("Введите число N: ");
+string? lineN = Console.ReadLine();
+
+if (lineN != null)
+{
+    int N = int.Parse(lineN);
+    int[] array = new int[N];
+    FillArray(array);
+    PrintEvenArray(array);
+}
+
