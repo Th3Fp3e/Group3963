d49dad79d77df716f50ec9d6ed1d957563a6a0b3
Th3_Fp3e <dartneket@yandex.ru>
2023-02-14 15:59:18 +0300

    Task 6 - odd or even

---
+++ Homework/Task 6/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число для проверки: ");
+string? lineNum = Console.ReadLine();
+
+if (lineNum != null)
+{
+    int num = int.Parse(lineNum);
+    if (num % 2 == 0)
+    {
+        Console.WriteLine("Ваше число четное!");
+    }
+    else
+    {
+        Console.WriteLine("Ваше число нечетное!");
+    }
+}
