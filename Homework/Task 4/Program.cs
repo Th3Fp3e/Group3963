ebf4b1fd779a2ef380957ca2ab650dc25dbf4c52
Th3_Fp3e <dartneket@yandex.ru>
2023-02-14 15:53:05 +0300

    Task 3 - max out of three

---
+++ Homework/Task 4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Сделаем функцию для поиска максимума из трех чисел
+
+int Max(int num1, int num2, int num3)
+{
+    int result = num1;
+    if (num2 > result) result = num2;
+    if (num3 > result) result = num3;
+    return result;
+}
+
+Console.Write("Введите первое число: ");
+string? lineNum1 = Console.ReadLine();
+Console.Write("Введите второе число: ");
+string? lineNum2 = Console.ReadLine();
+Console.Write("Введите третье число: ");
+string? lineNum3 = Console.ReadLine();
+
+if (lineNum1 != null && lineNum2 != null && lineNum3 != null)
+{
+    int num1 = int.Parse(lineNum1);
+    int num2 = int.Parse(lineNum2);
+    int num3 = int.Parse(lineNum3);
+    int max = Max (num1, num2, num3);
+    Console.Write("Максимальное значение - ");
+    Console.WriteLine(max);
+}
+
