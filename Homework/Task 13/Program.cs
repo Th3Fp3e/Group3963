179db182a94b1648d6873cc0403b59151d7f1d2d
Th3_Fp3e <dartneket@yandex.ru>
2023-02-18 22:18:36 +0300

    Task 13 (*)

--- Homework/Task 13/Program.cs
+++ Homework/Task 13/Program.cs
@@ -3,14 +3,36 @@
 // или сообщает, что третьей цифры нет.
 
 Console.WriteLine("Введите ваше число: ");
-string? num = Console.ReadLine();
 
-char[] arr = num.ToCharArray();
+// string? num = Console.ReadLine();
 
-if (arr.Length > 2)
+// char[] arr = num.ToCharArray();
+
+// if (arr.Length > 2)
+// {
+//     int thirdDigit = (int)arr[2] - 48;
+//     Console.WriteLine($"{thirdDigit} - третья цифра числа {num}");
+// }
+// else
+// {
+//     Console.WriteLine($"В числе {num} нет третьей цифры");
+// }
+
+int GetThirdDigit(int input)
 {
-    int thirdDigit = (int)arr[2] - 48;
-    Console.WriteLine($"{thirdDigit} - третья цифра числа {num}");
+    while (input > 999)
+    {
+        input = input / 10;
+    }
+    return input % 10;
+}
+
+int num = int.Parse(Console.ReadLine() ?? "0");
+
+if (num >= 100)
+{
+    int digit = GetThirdDigit(num);
+    Console.WriteLine($"{digit} - третья цифра числа {num}");
 }
 else
 {
