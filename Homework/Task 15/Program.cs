b5a710bcfeaeab0477c903e6a66032bb1e31581d
Th3_Fp3e <dartneket@yandex.ru>
2023-02-18 22:54:27 +0300

    Task 15 - weekend

---
+++ Homework/Task 15/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 15: 
+// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите число от 1 до 7: ");
+int weekDay = int.Parse(Console.ReadLine()??"0");
+
+bool IsItWeekend(int num)
+{
+    if (num > 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+if (weekDay > 0 && weekDay <= 7)
+{
+    if (IsItWeekend(weekDay))
+    {
+        Console.WriteLine("Сегодня выходной!");
+    }
+    else
+    {
+        Console.WriteLine("Боюсь, сегодня будний день");
+    }
+}
+else
+{
+    Console.WriteLine("Повторюсь, от 1 до 7!");
+}
+
