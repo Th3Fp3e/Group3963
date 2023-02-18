3f9a0e7ec341041697a376d4924df5863709f927
Th3_Fp3e <dartneket@yandex.ru>
2023-02-18 21:38:07 +0300

    Task 13 - third digit

---
+++ Homework/Task 13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: 
+// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите ваше число: ");
+string? num = Console.ReadLine();
+
+char[] arr = num.ToCharArray();
+
+if (arr.Length > 2)
+{
+    int thirdDigit = (int)arr[2] - 48;
+    Console.WriteLine($"{thirdDigit} - третья цифра числа {num}");
+}
+else
+{
+    Console.WriteLine($"В числе {num} нет третьей цифры");
+}
