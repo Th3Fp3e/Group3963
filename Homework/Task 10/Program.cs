a1fe6f808a98d88cc1b9d0d342da3e4320df449f
Th3_Fp3e <dartneket@yandex.ru>
2023-02-18 21:07:35 +0300

    Added comments

--- Homework/Task 10/Program.cs
+++ Homework/Task 10/Program.cs
@@ -4,10 +4,12 @@
 
 Console.Write("Введите трехзначное число: ");
 string? num = Console.ReadLine();
+
+// Переводим введенное число в массив
 char[] arr = num.ToCharArray();
 
 if (arr.Length >= 2)
 {
-    int secondDigit = (int)arr[1] - 48;
+    int secondDigit = (int)arr[1] - 48; // Выражение (<> - 48) переводит в int значение char (ASCII)
     Console.WriteLine($"{secondDigit} - вторая цифра числа {num}");
 }
