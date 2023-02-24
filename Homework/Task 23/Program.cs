f7635d7ed14df09bf3cf6498db149de50a333edf
Th3_Fp3e <dartneket@yandex.ru>
2023-02-24 08:41:43 +0300

    Task 23

---
+++ Homework/Task 23/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+// Creating a method to construct a line of all values from 1 to entered in set power
+string PowBuilder(int num, int pow)
+{
+    string res = string.Empty;
+    for (int i = 1; i <= num; i++)
+    {
+        res = res + Math.Pow(i, pow) + " ";
+    }
+    return res;
+}
+
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+int n = ReadData("Enter your value: ");
+
+Console.WriteLine(PowBuilder(n, 1));
+Console.WriteLine(PowBuilder(n, 3));
--- Sem4Task19/Program.cs
+++ Sem4Task19/Program.cs
@@ -1,6 +1,6 @@
 ﻿int ReadData (string msg)
 {
-    Console.WriteLine("Введите пятизначное числоЖ ");
-    return res = int.Parse(Console.ReadLine()??"0");
+    Console.WriteLine("Введите пятизначное число: ");
+    return int.Parse(Console.ReadLine()??"0");
 }
 
