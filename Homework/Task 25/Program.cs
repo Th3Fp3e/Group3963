6e29e661f436e38cccebd5b2c94cff628beab03a
Th3_Fp3e <dartneket@yandex.ru>
2023-02-28 00:04:49 +0300

    Task 27 - Sum of digits

--- Homework/Task 25/Program.cs
+++ Homework/Task 25/Program.cs
@@ -5,19 +5,19 @@
 int ReadData(string message)
 {
     Console.WriteLine(message);
-    return int.Parse(Console.ReadLine()??"0");
+    return int.Parse(Console.ReadLine() ?? "0");
 }
-                                    // Nothing unusual about those two
-void PrintData(string msg) 
+// Nothing unusual about those two
+void PrintData(string msg)
 {
     Console.WriteLine(msg);
 }
 
-double ToPower (int num, int pow)  // Using "double" instead of "int" to allow the displeay of higher results
+int ToPower(int num, int pow)  
 {
     int res = 1;
     if (pow == 0) return res;  // Beacause any number in power 0 equals 1! Math!!! 
-    else 
+    else
     {
         for (int i = 1; i <= pow; i++)  // Goisng with a simple cycle of multiplying by a set number
         {
@@ -27,9 +27,57 @@ double ToPower (int num, int pow)  // Using "double" instead of "int" to allow t
     return res;
 }
 
-int A = ReadData("Enter your number: ");
-int B = ReadData("Enter the power to raise it to: ");
+// int A = ReadData("Enter your number: ");
+// int B = ReadData("Enter the power to raise it to: ");
 
+// int result = ToPower(A, B);
+// PrintData($"{A}, raised to the power of {B}, equals {result}");
 
-double result = ToPower(A, B);
-PrintData($"{A}, raised to the power of {B}, equals {result}");
+// (*) Написать калькулятор с операциями +, -, /, * и возведение в степень
+
+// Adding a referense list of commands 
+string text = "List of commands: "
++ "1) Adding "
++ "2) Subtracting "
++ "3) Dividing "
++ "4) Multiplying "
++ "5) Raise to power";
+
+// Creating a base for our operations
+int Add(int num, int add)
+{
+    int res = num + add;
+    return res;
+}
+
+int Subtract(int num, int sub)
+{
+    int res = num - sub;
+    return res;
+}
+
+int Divide(int num, int div)
+{
+    int res = num / div;
+    return res;
+}
+
+int Multiply(int num, int mult)
+{
+    int res = num * mult;
+    return res;
+}
+
+int A = ReadData("Enter the first number: ");
+int B = ReadData("Enter the second number: ");
+Console.WriteLine(text);
+int command = ReadData("Enter the desired command (from 1 to 5): ");
+
+int result = 0;
+if (command == 1) result = Add(A, B);
+if (command == 2) result = Subtract(A, B);
+if (command == 3) result = Divide(A, B);
+if (command == 4) result = Multiply(A, B);
+if (command == 5) result = ToPower(A, B);
+
+PrintData($"The result of entered command is {result}");
---
+++ Homework/Task 27/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 27: 
+// Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+// Starting with the good ond one-two combo
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+void PrintData(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+// Making a method for summing the digits...
+int SumOfDigits (int num)
+{
+    int res = 0; // ...by starting at zero...
+    while (num >0)
+    {
+        res += num % 10; // ...then adding the last digit (as %10 leaves exactly the last digit)...
+        num = num / 10; // ...and ending with shortening the OG number by 10 before continuing the cycle!
+    }
+    return res;
+}
+
+int input = ReadData("Enter yor value: ");
+// Shortening the code a bit here by putting the calculations directly into the output
+PrintData($"The sum of digits of your value {input} equals to {SumOfDigits(input)}");
