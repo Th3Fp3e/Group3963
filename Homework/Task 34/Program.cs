a8c6c83be689971ffc449b643d7ee75a77bf1764
Th3_Fp3e <dartneket@yandex.ru>
2023-03-03 19:01:12 +0300

    Task 34 - Evens and Sortings

--- Homework/Task 34/Program.cs
+++ Homework/Task 34/Program.cs
@@ -2,6 +2,9 @@
 //Задайте массив заполненный случайными положительными трёхзначными числами. 
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 
+
+// Starting with two methods to generate and print a random array
+
 int[] GetArray(int length, int minVal, int maxVal)
 {
     int[] array = new int[length];
@@ -14,12 +17,12 @@ int[] GetArray(int length, int minVal, int maxVal)
 
 void Print1DArray(int[] array)
 {
-    Console.Write("[");  
+    Console.Write("[");
     for (int j = 0; j < array.Length - 1; j++)
     {
-        Console.Write($"{array[j]}, "); 
+        Console.Write($"{array[j]}, ");
     }
-    Console.WriteLine($"{array[array.Length - 1]}]");  
+    Console.WriteLine($"{array[array.Length - 1]}]");
 }
 
 int ReadData(string message)
@@ -33,14 +36,54 @@ void PrintData(string msg)
     Console.WriteLine(msg);
 }
 
+
+// An easy method to calculate an amount of even values in an array
+
 int VarNaive(int[] inputArray)
 {
     int result = 0;
     for (int i = 0; i < inputArray.Length; i++)
     {
-        if (inputArray[i] % 2 == 0) result ++;
+        if (inputArray[i] % 2 == 0) result++;
     }
     return result;
+}
+
+// Crerating a bubble sorting method...
+
+void BubbleSort(int[] arr)
+{
+    int temp = 0;
+    for (int j = 0; j < arr.Length; j++) // This will sort through the array multiple times to prevent errors
+    {
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                temp = arr[i];       //
+                arr[i] = arr[i + 1];   // A simple bubble sort with a temp variable
+                arr[i + 1] = temp;     //
+            }
+        }
+    }
+}
+
+// ... and an insertion sorting method
+
+void InsertSort(int[] arr)
+{
+    for (int i = 1; i < arr.Length; i++)
+    {
+        int x = arr[i];
+        int j = i;
+        while (j > 0 && arr[j - 1] > x)
+        {
+            arr[j] = arr[j - 1];
+            arr[j - 1] = x;
+            j = j - 1;
+        }
+        arr[j] = x;
+    }
 }
 
 int N = ReadData("Enter the desired length: ");
@@ -50,3 +93,18 @@ int[] testArray = GetArray(N, min, max);
 Print1DArray(testArray);
 int result = VarNaive(testArray);
 PrintData(result.ToString());
+
+
+// We'll make a bubble sort and check, how much time it takes
+
+DateTime t1 = DateTime.Now;
+BubbleSort(testArray);
+Print1DArray(testArray);
+Console.WriteLine(DateTime.Now - t1);
+
+// And we'll do the same with Insert-sorting
+
+DateTime t2 = DateTime.Now;
+InsertSort(testArray);
+Print1DArray(testArray);
+Console.WriteLine(DateTime.Now - t2);
