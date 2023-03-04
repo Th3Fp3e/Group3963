282a9190b1c9997ecae5fe74a2e067f40781c6c2
Th3_Fp3e <dartneket@yandex.ru>
2023-03-05 00:58:49 +0300

    Homework 6 - middle point

---
+++ Homework/Task 41/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 41: 
+// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int ReadData(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+int[] ReadMessage(int length)
+{
+    int[] message = new int[length];
+    for (int i = 0; i < message.Length; i++)
+    {
+        message[i] = ReadData($"Please input the {i+1} element: ");
+    }
+    return message;
+}
+
+int CountThePositives(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    return count;
+}
+
+void PrintData(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+
+int length = ReadData("Enther the desired length: ");
+int[] inputLine = ReadMessage(length);
+int result = CountThePositives(inputLine);
+PrintData($"The amount of positive numbers you have enetered is {result}");
---
+++ Homework/Task 43/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 43: 
+// Напишите программу, которая найдёт точку 
+// пересечения двух прямых, заданных уравнениям
+
+int ReadData(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+void PrintData(string msg)
+{
+    Console.WriteLine(msg);
+}
+
