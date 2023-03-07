231febf967216198ee5fc5661cdaaab127f7b3bf
Th3_Fp3e <dartneket@yandex.ru>
2023-03-07 21:28:38 +0300

    Task 50 - Fibonacci

--- Homework/Task 50/Program.cs
+++ Homework/Task 50/Program.cs
@@ -9,20 +9,52 @@ int ReadData(string message)
     return int.Parse(Console.ReadLine() ?? "0");
 }
 
-int[,] Gen2DArray(int countRows, int countColumn, int max)
+// That's a generic array generator
+
+// int[,] Gen2DArray(int countRows, int countColumn, int max)
+// {
+//     int[,] arr = new int[countRows, countColumn];
+//     for (int i = 0; i < countRows; i++)
+//     {
+//         for (int j = 0; j < countColumn; j++)
+//         {
+//             arr[i, j] = new Random().Next(10, max + 1);
+//         }
+//     }
+//     return arr;
+// }
+
+long FiboNum(int n)
 {
-    int[,] arr = new int[countRows, countColumn];
-    for (int i = 0; i < countRows; i++)
+    long res = 1;
+    if (n ==1 || n ==2) return res;
+    long prev = 1;
+    long curr = 1;
+    for (int i = 2; i <= n; i++)
     {
-        for (int j = 0; j < countColumn; j++)
+        res = curr+prev;
+        prev = curr;
+        curr = res;
+    }
+    return res;
+}
+
+long[,] Gen2DFiboArray(int rows, int col)
+{
+    long[,] arr = new long[rows, col];
+    int count =1;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
         {
-            arr[i, j] = new Random().Next(10, max + 1);
+            arr[i,j] = FiboNum(count);
+            count++;
         }
     }
     return arr;
 }
 
-void Print2DArr(int[,] arr)
+void Print2DArr(long[,] arr)
 {
     for (int i = 0; i < arr.GetLength(0); i++)
     {
@@ -34,18 +66,18 @@ void Print2DArr(int[,] arr)
     }
 }
 
-bool IsItThere(int[,] arr, int row, int col)
+bool IsItThere(long[,] arr, int row, int col)
 {
-    if (row < arr.GetLength(0) && col<arr.GetLength(1)) return true;
+    if (row-1 < arr.GetLength(0) && col-1<arr.GetLength(1)) return true;
     else return false;
 }
 
-void Print2DArrayElem(int[,] arr, int row, int col)
+void Print2DArrayElem(long[,] arr, int row, int col)
 {
-    Console.WriteLine($"Your element is {arr[row, col]}");
+    Console.WriteLine($"Your element is {arr[row-1, col-1]}");
 }
 
-int[,] testArr = Gen2DArray (13, 20, 100);
+long[,] testArr = Gen2DFiboArray (6, 8);
 int inRow = ReadData("Enter the row of the element to search: ");
 int inCol = ReadData("Enter the column of the element to search: ");
 
