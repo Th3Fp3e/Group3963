cef82742775e16f00222b575ee14d33752f3eabe
Th3_Fp3e <dartneket@yandex.ru>
2023-03-07 20:35:51 +0300

    Homework 7 - middle point

---
+++ Homework/Task 47/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 47. 
+// Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+
+int ReadData(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+int[,] Gen2DArray (int rows, int col, int max)
+{
+    int[,] arr = new int[rows, col];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = new Random().Next(10, max+1);
+        }
+    }
+    return arr;
+}
+
+void Print2DArr(int[,] arr)
+{
+    ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.ForegroundColor = col[new Random().Next(0,16)];
+            Console.Write(arr[i, j] + " ");
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+    }
+}
+
+int inputRows = ReadData("Enter the desired amount of rows: ");
+int inputColumns = ReadData("Enter the desired amount of columns: ");
+
+int[,] testArr = Gen2DArray(inputRows, inputColumns, 100);
+Print2DArr(testArr);
---
+++ Homework/Task 50/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 50. 
+// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве
+// , и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+
+int ReadData(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+int[,] Gen2DArray(int countRows, int countColumn, int max)
+{
+    int[,] arr = new int[countRows, countColumn];
+    for (int i = 0; i < countRows; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            arr[i, j] = new Random().Next(10, max + 1);
+        }
+    }
+    return arr;
+}
+
+void Print2DArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+bool IsItThere(int[,] arr, int row, int col)
+{
+    if (row < arr.GetLength(0) && col<arr.GetLength(1)) return true;
+    else return false;
+}
+
+void Print2DArrayElem(int[,] arr, int row, int col)
+{
+    Console.WriteLine($"Your element is {arr[row, col]}");
+}
+
+int[,] testArr = Gen2DArray (13, 20, 100);
+int inRow = ReadData("Enter the row of the element to search: ");
+int inCol = ReadData("Enter the column of the element to search: ");
+
+Print2DArr(testArr);
+
+if (IsItThere(testArr, inRow, inCol))
+{
+    Print2DArrayElem(testArr, inRow, inCol);
+}
+else Console.WriteLine("There's no such element. Try different index.");
