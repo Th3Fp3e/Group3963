ea1d994c536caf72ff620af63ce7e1a969088f28
Th3_Fp3e <dartneket@yandex.ru>
2023-03-17 11:26:26 +0300

    IT'S A FINAL HOMEWORK!!!

--- Homework/Task 68/Program.cs
+++ Homework/Task 68/Program.cs
@@ -8,18 +8,22 @@ int ReadData(string message)
     return int.Parse(Console.ReadLine() ?? "0");
 }
 
+// Enetring the sequance for calculating Ackermann function
 int RecAckerMN(int m, int n)
 {
+    // A(0,n) = n+1
     if (m == 0)
     {
         return n + 1;
     }
     else
     {
+        // A(m,0) = A(m-1,1)
         if (m != 0 && n == 0)
         {
             return RecAckerMN(m - 1, 1);
         }
+        // A(m,n) = A(m-1,A(m,n-1))
         else
         {
             return RecAckerMN(m - 1, RecAckerMN(m, n - 1));
